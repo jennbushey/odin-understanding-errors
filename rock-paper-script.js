5a4bb1a5972e9fbaf4f70a9619a6f102ce025e67
Jenn Bushey <117766897+jennbushey@users.noreply.github.com>
2023-03-17 15:51:04 -0600

    RPS plays but does not yet alert the correct information eg player choice beats computer choice

--- rock-paper-script.js
+++ rock-paper-script.js
@@ -1,61 +1,61 @@
-function getComputerChoice () {
-    let choice = Math.random();
-    if (choice <= 1/3) {
-        choice = "rock";
-    } else if (choice <= 2/3) {
-        choice = "paper";
-    } else {choice = "scissors"}
+const playerSelection = document.getElementById("myBtn")
+playerSelection.addEventListener("click",playRound);
+
+function getComputerChoice() {
+    let choice = Math.random(); // generates random number between 0 and 1
+    if (choice <= 1 / 3) {
+        choice = "rock"; // rock if < 1/3
+    } else if (choice <= 2 / 3) {
+        choice = "paper"; // paper if between 1/3 and 2/3
+    } else { choice = "scissors" } // scissors if >2/3
     return choice;
 };
 
+function playRound(playerSelection, computerSelection) {
+    computerSelection = getComputerChoice();
+    console.log(playerSelection,computerSelection);
 
-function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase();
-
- 
-    if (playerSelection === computerSelection){
-        round++;
-        tie++;
-        return "Tie game. We both guessed " + playerSelection + ".";
-    } 
-    else if ((playerSelection === "rock") && (computerSelection === "scissors")){
-        round++;
-        win++;
-        return "You win! " + playerSelection + " beats " + computerSelection + ".";
-    } 
-    else if ((playerSelection === "paper") && (computerSelection === "rock")){   
-        round++;
-        win++;
-        return "You win! " + playerSelection + " beats " + computerSelection + ".";
+    if (playerSelection === computerSelection) {
+        //round++;
+        //tie++;
+        return alert("Tie game. We both guessed " + playerSelection + ".");
     }
-    else if ((playerSelection === "scissors") && (computerSelection === "paper")){
-        round++;
-        win++;
-        return "You win! " + playerSelection + " beats " + computerSelection + ".";
+    else if ((playerSelection === "rock") && (computerSelection === "scissors")) {
+        //round++;
+        //win++;
+        return alert("You win! " + playerSelection + " beats " + computerSelection + ".");
+    }
+    else if ((playerSelection === "paper") && (computerSelection === "rock")) {
+        // round++;
+        // win++;
+        return alert("You win! " + playerSelection + " beats " + computerSelection + ".");
+    }
+    else if ((playerSelection === "scissors") && (computerSelection === "paper")) {
+        // round++;
+        // win++;
+        return alert("You win! " + playerSelection + " beats " + computerSelection + ".");
     }
     else {
-        round++;
-        lose++;
-        return "You lose! " + computerSelection + " beats " + playerSelection + ".";
+        // round++;
+        // lose++;
+        return alert("You lose! " + computerSelection + " beats " + playerSelection + ".");
     }
 }
 
-
-function game(){
-    for (let game = round; game < 5; game++){
-        const playerSelection = prompt("Please enter your Rock Paper Scissors choice: ");
-        if (!(playerSelection === "rock") && !(playerSelection === "paper") && !(playerSelection === "scissors")){
-            game--;
-            continue;
-        }
-        const computerSelection = getComputerChoice();
-        alert(playRound(playerSelection,computerSelection));    
-    }
-    alert("Final Score: " + win + " wins, " + lose + " losses, and " + tie + " tie games.")
-}
-
+/*
 let round = 0;
 let win = 0;
 let lose = 0;
 let tie = 0;
-game();
+*/
+
+    //for (let game = round; game < 1; game++){
+    //const playerSelection = prompt("Please enter your Rock Paper Scissors choice: ");
+    //if (!(playerSelection === "rock") && !(playerSelection === "paper") && !(playerSelection === "scissors")) {
+    //    game--;
+    //    continue;
+    //}
+    //const computerSelection = getComputerChoice();
+    //alert(playRound(playerSelection, computerSelection));
+    // }
+    //alert("Final Score: " + win + " wins, " + lose + " losses, and " + tie + " tie games.")
