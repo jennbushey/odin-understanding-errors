4fd0b5bb9958fed7a682e4ec110f02273cbaa6b2
Jenn Bushey <117766897+jennbushey@users.noreply.github.com>
2023-03-17 18:46:35 -0600

    Displays win/lose/tie for last round using DOM and <div>.

--- rock-paper-script.js
+++ rock-paper-script.js
@@ -4,12 +4,14 @@ const btn2 = document.querySelector(".paper");
 const btn3 = document.querySelector(".scissors");
 let playerSelection;
 
-//console.log(btn1,btn2,btn3);
+
+const container = document.querySelector('#rpsGame');
+const displayResult = document.createElement('h3');
 
 btn1.addEventListener('click', function (e) {
     playerSelection = "rock";
     //console.log(`I picked ${playerSelection}`);
-    playRound(playerSelection);
+    showResults(playRound(playerSelection));
 });
 
 btn2.addEventListener('click', function (e) {
@@ -31,85 +33,33 @@ function getComputerChoice() {
     } else if (choice <= 2 / 3) {
         choice = "paper"; // paper if between 1/3 and 2/3
     } else { choice = "scissors" } // scissors if >2/3
-    //console.log(`Computer chooses ${choice}`);
+    console.log(`Computer chooses ${choice}`);
     return choice;
 };
 
 function playRound(playerSelection) {
     let computerSelection = getComputerChoice();
+
     //console.log(playerSelection, computerSelection);
     if (playerSelection === computerSelection) {
-        return console.log("Tie game."), playerSelection, computerSelection;
-    } 
+        return "Tie game.";
+    }
     else if ((playerSelection === "rock") && (computerSelection === "scissors")) {
-        return console.log("You win!"), playerSelection, computerSelection;
-    } 
+        return "You win.";
+    }
     else if ((playerSelection === "paper") && (computerSelection === "rock")) {
-        return console.log("You win!"), playerSelection, computerSelection;
-    } 
+        return "You win.";
+    }
     else if ((playerSelection === "scissors") && (computerSelection === "paper")) {
-        return console.log("You win!"), playerSelection, computerSelection;
-    } 
+        return "You win.";
+    }
     else {
-        return console.log("You lose."), playerSelection, computerSelection;
+        return "You lose.";
     }
 };
 
-/*
-    const rpsGame = document.querySelector('#rpsGame')
-
-
-if (playerSelection === computerSelection) return {
-round++;
-//tie++;
-//return alert("Tie game. We both guessed " + playerSelection + ".");
-result = "Tie game."
-/*const div = document.createElement('div');
-div.classList.add('displayResults');
-div.textContent = 'Tie Game.';
-rpsGame.appendChild(div);
-return result;
-}
-else if ((playerSelection === "rock") && (computerSelection === "scissors")) {
-round++;
-win++;
-//return alert("You win! " + playerSelection + " beats " + computerSelection + ".");
-result = "You win!"
-return result, playerSelection, computerSelection;
-}
-else if ((playerSelection === "paper") && (computerSelection === "rock")) {
-round++;
-win++;
-//return alert("You win! " + playerSelection + " beats " + computerSelection + ".");
-result = "You win!"
-return result, playerSelection, computerSelection;
-}
-else if ((playerSelection === "scissors") && (computerSelection === "paper")) {
-round++;
-win++;
-//return alert("You win! " + playerSelection + " beats " + computerSelection + ".");
-result = "You win!"
-return result, playerSelection, computerSelection;
-}
-else {
-round++;
-lose++;
-//return alert("You lose! " + computerSelection + " beats " + playerSelection + ".");
-result = "You lose."
-return result, playerSelection, computerSelection;
-}
-
-}
-*/
-
-//for (let game = round; game < 1; game++){
-//const playerSelection = prompt("Please enter your Rock Paper Scissors choice: ");
-//if (!(playerSelection === "rock") && !(playerSelection === "paper") && !(playerSelection === "scissors")) {
-//    game--;
-//    continue;
-//}
-//const computerSelection = getComputerChoice();
-//alert(playRound(playerSelection, computerSelection));
-// }
-//alert("Final Score: " + win + " wins, " + lose + " losses, and " + tie + " tie games.")
-
+function showResults(result) {
+    displayResult.classList.add('result');
+    displayResult.textContent = result;
+    rpsGame.appendChild(displayResult);
+};
